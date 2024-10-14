c07087f42856c0979d1d434260ca0569ed6b9659
Neha Kamlesh <141848836+nehakamlesh06@users.noreply.github.com>
2024-10-14 21:56:15 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,86 @@
+const todoForm = document.querySelector("form");
+const todoInput = document.getElementById('todo-input');
+const todoListUL = document.getElementById('todo-list');
+
+let allTodos = getTodos();
+updateTodoList();
+
+
+todoForm.addEventListener('submit',function(e){
+    e.preventDefault();
+    addTodo();   
+})
+
+function addTodo(){
+    const todoText= todoInput.value.trim();
+    console.log("add todo is working")
+    console.log(todoText)
+
+    if(todoText.length>0){
+        allTodos.push(todoText);
+        updateTodoList();
+        saveTodo();
+        todoInput.value="";
+        
+    }
+    console.log(allTodos)
+}
+
+function updateTodoList(){
+    todoListUL.innerHTML="";
+    allTodos.forEach((todo, todoIndex) => {
+        todoItem = createTodoItem(todo, todoIndex);
+        todoListUL.append(todoItem);
+        
+    })
+}
+
+function createTodoItem(todo, todoIndex){
+    const todoID= "todo-"+todoIndex;
+    const todoLI= document.createElement("li");
+    todoLI.className = "todo";
+    todoLI.innerHTML = `
+    <input type="checkbox" id="${todoID}">
+            <label class="custom-checkbox" for="${todoID}">
+                <svg xmlns="http://www.w3.org/2000/svg" color="black" height="24px" viewBox="0 -960 960 960" fill="#e8eaed" width="24px"><path d="M382-240 154-468l57-57 171 171 367-367 57 57-424 424Z"/></svg>
+            </label>
+            <label for="${todoID}" class="todo-text">
+                ${todo}
+            </label>
+            <button class="delete-button">
+                <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" fill="#e8eaed" width="24px" ><path d="M280-120q-33 0-56.5-23.5T200-200v-520h-40v-80h200v-40h240v40h200v80h-40v520q0 33-23.5 56.5T680-120H280Zm400-600H280v520h400v-520ZM360-280h80v-360h-80v360Zm160 0h80v-360h-80v360ZM280-720v520-520Z"/></svg>
+            </button>`
+    
+    
+        const deleteButton = todoLI.querySelector(".delete-button");
+        deleteButton.addEventListener("click",() =>{
+            deleteTodoItem(todoIndex)
+        })
+        
+    
+    
+    
+    
+    
+    
+    
+    
+    
+             return todoLI;       
+}
+function saveTodo(){
+    const todosJson = JSON.stringify(allTodos);
+    localStorage.setItem("todos",todosJson);
+}
+function getTodos(){
+    const todos = localStorage.getItem("todos") || "[]";
+    return JSON.parse(todos);
+}
+
+
+function deleteTodoItem(todoIndex){
+    allTodos = allTodos.filter((_, i)=> i !== todoIndex);
+    saveTodo();
+    updateTodoList();
+    
+}
